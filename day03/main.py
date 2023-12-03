63f3af3a85110534665f75fbb1e85b4ad3034030
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-03 11:00:41 +0000

    Day 3.2

--- day03/main.py
+++ day03/main.py
@@ -3,7 +3,6 @@ import re
 
 part_numbers = []
 symbols = []
-set({})
 
 x = 0
 for input in fileinput.input():
@@ -12,31 +11,50 @@ for input in fileinput.input():
     for match in re.finditer("\d+", schematic_line):
         part_numbers.append(
             {
-                "part_number": match.group(),
+                "part_number": int(match.group()),
                 "positions": {(x, y) for y in range(match.start(), match.end())},
             }
         )
 
     for match in re.finditer("[^\d\.]", schematic_line):
-        symbols.append((x, match.start()))
+        symbols.append(
+            {
+                "symbol": match.group(),
+                "position": (x, match.start()),
+            }
+        )
 
     x += 1
 
-adjancies = {
+
+def generate_adjacancies(x, y):
+    return [(a_x, a_y) for a_x in range(x - 1, x + 2) for a_y in range(y - 1, y + 2)]
+
+
+adjacancies = {
     (x, y)
     for symbol in symbols
-    for x in range(symbol[0] - 1, symbol[0] + 2)
-    for y in range(symbol[1] - 1, symbol[1] + 2)
+    for (x, y) in generate_adjacancies(symbol["position"][0], symbol["position"][1])
 }
 
 true_part_numbers = [
-    int(part_number["part_number"])
+    part_number["part_number"]
     for part_number in part_numbers
-    if len(part_number["positions"].intersection(adjancies)) > 0
+    if len(part_number["positions"].intersection(adjacancies)) > 0
 ]
 
-print(part_numbers)
-print(symbols)
-print(adjancies)
-print(true_part_numbers)
 print(sum(true_part_numbers))
+
+
+gear_ratio_total = 0
+for symbol in symbols:
+    if symbol["symbol"] == "*":
+        adj = generate_adjacancies(symbol["position"][0], symbol["position"][1])
+        adjacent_parts = []
+        for part_number in part_numbers:
+            if len(part_number["positions"].intersection(adj)) > 0:
+                adjacent_parts.append(part_number["part_number"])
+        if len(adjacent_parts) == 2:
+            gear_ratio_total += adjacent_parts[0] * adjacent_parts[1]
+
+print(gear_ratio_total)
