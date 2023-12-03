cd4fe1321c81bf719d2963a0118b19e319c89036
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-03 11:00:41 +0000

    Day 1.2

--- day01/main.py
+++ day01/main.py
@@ -1,17 +1,23 @@
 import fileinput
 import re
 
-# run from command line a:
+# run from command line as:
 #   python3 main.py < sample.txt
 
-digit_lists = [
-    re.findall("\d", i)
+words_replaced = [
+    i.replace("one", "o1e")
+    .replace("two", "t2o")
+    .replace("three", "th3ee")
+    .replace("four", "f4ur")
+    .replace("five", "f5ve")
+    .replace("six", "s6x")
+    .replace("seven", "se7en")
+    .replace("eight", "ei8ht")
+    .replace("nine", "n9ne")
+    .replace("zero", "z0ro")
     for i in fileinput.input()
 ]
 
-calibrations = [
-    int(''.join(d[0:1] + d[-1::]))
-    for d in digit_lists
-]
-
+digit_lists = [re.findall("\d", w) for w in words_replaced]
+calibrations = [int("".join(d[0:1] + d[-1::])) for d in digit_lists]
 print(sum(calibrations))
