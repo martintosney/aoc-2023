1d6ba857b82923751aa34dd160048d2466246411
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-03 11:00:41 +0000

    Reformat

--- day02/main.py
+++ day02/main.py
@@ -1,24 +1,22 @@
 import fileinput
-from functools import reduce 
+from functools import reduce
 import re
 
 # run from command line as:
 #   python3 main.py < sample.txt
 
-NUM_CUBES = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
+NUM_CUBES = {"red": 12, "green": 13, "blue": 14}
+
 
 def parse_game_number(game_number_text: str):
-    return int(game_number_text.split(' ')[1])
+    return int(game_number_text.split(" ")[1])
+
 
 def parse_games(games_text: str):
     cube_results = [
-        cube_text.split(' ')
-        for game in games_text.split('; ')
-        for cube_text in game.split(', ')
+        cube_text.split(" ")
+        for game in games_text.split("; ")
+        for cube_text in game.split(", ")
     ]
 
     max_results = {}
@@ -33,20 +31,23 @@ def parse_games(games_text: str):
 
     return max_results
 
+
 def parse_game(game_string: str):
-    game_number_text, game_results_text = game_string.split(': ')
+    game_number_text, game_results_text = game_string.split(": ")
 
     return {
         "game_number": parse_game_number(game_number_text),
-        "totals": parse_games(game_results_text)
+        "totals": parse_games(game_results_text),
     }
 
+
 def is_valid_game(game_result, available_cubes):
-    for (colour, count) in game_result["totals"].items():
+    for colour, count in game_result["totals"].items():
         if available_cubes.get(colour, 0) < count:
             return False
 
     return True
+
 
 def sum_possible_games(game_strings):
     possible_games = [
@@ -57,18 +58,16 @@ def sum_possible_games(game_strings):
 
     return sum(possible_games)
 
+
 input = [i.strip() for i in fileinput.input()]
 print(sum_possible_games(input))
 
+
 def sum_powers(game_strings):
-    games = [parse_game(game_string).get("totals").values() for game_string in game_strings]
-    return sum([
-        reduce(
-            lambda x, y: x * y,
-            game,
-            1
-        )
-        for game in games
-    ])
+    games = [
+        parse_game(game_string).get("totals").values() for game_string in game_strings
+    ]
+    return sum([reduce(lambda x, y: x * y, game, 1) for game in games])
+
 
 print(sum_powers(input))
