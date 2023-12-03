fa1d628b846b76ff08b12e2f3b71b7885a519d0b
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-03 16:49:45 +0000

    Day 4.2

--- day04/main.py
+++ day04/main.py
@@ -2,14 +2,30 @@ import fileinput
 import re
 
 
-def calc_score(card_text):
+def determine_matches(card_text):
     match = re.match("Card +\d+:(( +\d+)+) \|(( +\d+)+)", card_text)
     winning_numbers = set([int(i) for i in match.group(1).split(" ") if i != ""])
     card_numbers = set([int(i) for i in match.group(3).split(" ") if i != ""])
-    matching_nums = len(card_numbers.intersection(winning_numbers))
+    return len(card_numbers.intersection(winning_numbers))
+
+
+def calc_score(card_text):
+    matching_nums = determine_matches(card_text)
     return 0 if matching_nums == 0 else 2 ** (matching_nums - 1)
 
 
 input = [i.strip() for i in fileinput.input()]
 
 print(sum([calc_score(i) for i in input]))
+
+max_card = len(input)
+
+card_copies = {i: 1 for i in range(1, max_card + 1)}
+
+for card_num in range(1, max_card + 1):
+    num_matches = determine_matches(input[card_num - 1])
+
+    for card_won in range(card_num + 1, min(card_num + num_matches, max_card) + 1):
+        card_copies[card_won] += card_copies[card_num]
+
+print(sum(card_copies.values()))
