9bf6d5d1fa6d3155ad143ed22f000d3f65103064
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-05 10:31:51 +0000

    Day 5.2 - much better :)

--- day05/main.py
+++ day05/main.py
@@ -28,8 +28,11 @@ def parse_almanac(almanac_text):
                 int(seed.group()) for seed in re.finditer("\d+", almanac_line)
             ]
             maps.setdefault(map_from, {}).setdefault("mappings", []).append(
-                # Start        End                          Delta to apply
-                (source_start, source_start + range_length, target_start - source_start)
+                (
+                    source_start,  # Start
+                    source_start + range_length - 1,  # End
+                    target_start - source_start,  # Delta to apply
+                )
             )
 
     return (seeds, maps)
@@ -38,8 +41,6 @@ def parse_almanac(almanac_text):
 input = [i.strip() for i in fileinput.input()]
 
 (seeds, maps) = parse_almanac(input)
-
-print(maps)
 
 locations = []
 for seed in seeds:
@@ -55,4 +56,31 @@ for seed in seeds:
         resource_type = maps[resource_type]["target"]
     locations.append(current_value)
 
-print(min(locations))
+print(f"Part 1: {min(locations)}")
+
+
+resource_type = "seed"
+resource_ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
+
+while resource_type != "location":
+    unmodified_ranges = resource_ranges
+    modified_ranges = []
+
+    for m in maps[resource_type]["mappings"]:
+        next_batch = []
+        for r in unmodified_ranges:
+            if r[0] < m[0]:
+                next_batch.append((r[0], min(r[1], m[0] - 1)))
+            if r[1] >= m[0] and r[0] <= m[1]:
+                modified_ranges.append((max(r[0], m[0]) + m[2], min(r[1], m[1]) + m[2]))
+            if r[1] > m[1]:
+                next_batch.append((max(r[0], m[1] + 1), r[1]))
+        unmodified_ranges = next_batch
+
+    resource_ranges = unmodified_ranges + modified_ranges
+    resource_type = maps[resource_type]["target"]
+
+
+min_location = min([r[0] for r in resource_ranges])
+
+print(f"Part 2: {min_location}")
