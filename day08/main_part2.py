0d93a284c3d00230a51bf51d4719440dfefcca91
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-07 17:08:45 +0000

    Day 8.2 - Attempt 2

--- day08/main_part2.py
+++ day08/main_part2.py
@@ -1,7 +1,9 @@
 import fileinput
+import itertools
 import re
 
 input = [i.strip() for i in fileinput.input()]
+directions_text = input[0]
 
 
 def direction_generator(directions):
@@ -10,28 +12,52 @@ def direction_generator(directions):
             yield d
 
 
-directions = direction_generator(input[0])
-
 routes = {
     m.group(1): {"L": m.group(2), "R": m.group(3)}
     for m in [re.match("^(.*) = \((.*), (.*)\)", i) for i in input[2::]]
 }
 
-locations = [r for r in routes.keys() if r.endswith("A")]
-steps = 0
-loop = True
-while loop:
-    next_direction = next(directions)
-    steps += 1
-    next_round = []
-    loop = False
-    for l in locations:
-        new_location = routes[l][next_direction]
-        next_round.append(new_location)
-        if not new_location.endswith("Z"):
-            loop = True
-    locations = next_round
+
+def find_route(start_location, routes, directions_text):
+    directions = direction_generator(directions_text)
+    location = start_location
+    route = []
+    while not location.endswith("Z"):
+        location = routes[location][next(directions)]
+        route.append(location)
+    location = routes[location][next(directions)]
+    initial_offset = route.index(location)
+    loop_length = len(route) - initial_offset
+    return (initial_offset, loop_length)
 
 
+def generate_termination_states(initial_offset, loop_length):
+    position = initial_offset
+    while True:
+        position += loop_length
+        yield position
 
-print(steps)
+
+route_props = [
+    find_route(location, routes, directions_text)
+    for location in routes.keys()
+    if location.endswith("A")
+]
+
+
+def find_common_route_length(route_props):
+    highest_loop_length_index = max(enumerate(route_props), key=lambda x: x[1][1])[0]
+    route_iter = route_props.pop(highest_loop_length_index)
+
+    for route_length in generate_termination_states(*route_iter):
+        routes_match = True
+        for other_route_offset, other_route_loop_length in route_props:
+            if (route_length - other_route_offset) % other_route_loop_length != 0:
+                routes_match = False
+                break
+
+        if routes_match:
+            return route_length
+
+
+print(find_common_route_length(route_props))
