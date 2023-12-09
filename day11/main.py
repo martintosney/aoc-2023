68e32fc204146a32e09f90a93995e95e521a9a3b
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2023-12-09 16:06:19 +0000

    Day 11.2

--- day11/main.py
+++ day11/main.py
@@ -1,41 +1,50 @@
 import fileinput
 
 
-def expand_universe(starting_universe):
+def get_universe_expansions(universe):
     empty_cols = []
-    for x in reversed(range(len(starting_universe[0]))):
-        if not [y[x] for y in starting_universe if y[x] != "."]:
+    for x in range(len(universe[0])):
+        if not [y[x] for y in universe if y[x] != "."]:
             empty_cols.append(x)
 
-    expanded_universe = []
-    for row in starting_universe:
-        new_row = row.copy()
-        for col in empty_cols:
-            new_row.insert(col, ".")
-        expanded_universe.append(new_row)
-
-    for y, row in reversed(list(enumerate(expanded_universe))):
+    empty_rows = []
+    for y, row in enumerate(universe):
         if not [c for c in row if c != "."]:
-            expanded_universe.insert(y, row)
+            empty_rows.append(y)
 
-    return expanded_universe
+    return (empty_cols, empty_rows)
+
+
+def sum_lengths(universe, ratio=2):
+    galaxies = [
+        (x, y)
+        for y, row in enumerate(universe)
+        for x, symbol in enumerate(row)
+        if symbol == "#"
+    ]
+
+    galaxies = [(x, y) for x, y in galaxies]
+
+    (expand_cols, expand_rows) = get_universe_expansions(universe)
+
+    total_length = 0
+    for i, (x1, y1) in enumerate(galaxies):
+        for x2, y2 in galaxies[i + 1 :]:
+            delta_x = abs(x2 - x1) + (
+                (ratio - 1)
+                * len([x for x in expand_cols if min(x1, x2) < x < max(x1, x2)])
+            )
+            delta_y = abs(y2 - y1) + (
+                (ratio - 1)
+                * len([y for y in expand_rows if min(y1, y2) < y < max(y1, y2)])
+            )
+            total_length += delta_x + delta_y
+    return total_length
 
 
 input = [list(i.strip()) for i in fileinput.input()]
 
-uni = expand_universe(input)
 
-galaxies = [
-    (x, y) for y, row in enumerate(uni) for x, symbol in enumerate(row) if symbol == "#"
-]
+print(f"Part 1: {sum_lengths(input)}")
 
-total_length = 0
-for i, (x1, y1) in enumerate(galaxies):
-    for x2, y2 in galaxies[i + 1 :]:
-        total_length += abs(x2 - x1) + abs(y2 - y1)
-
-for u in uni:
-    print("".join(u))
-
-
-print(total_length)
+print(f"Part 2: {sum_lengths(input, 1000000)}")
